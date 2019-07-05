98c3d274e1c61e217ad5d0bdc605874293eae63c
peterk <tiglek@gmail.com>
2019-07-06 05:47:45 +0900

    START

---
+++ precache-manifest.731d48148535c70c46727d83031a3178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70689578fd329210d85e77dbb269d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a52d4b073da8490b07",
+    "url": "/static/css/main.caffe066.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c0a52d4b073da8490b07",
+    "url": "/static/js/main.233896f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f89c5e0a4015ec744e18f8167d1e0e9",
+    "url": "/static/media/ai-styler-logo.5f89c5e0.svg"
+  }
+]);
