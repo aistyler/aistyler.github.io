52784851aac55957de32846f53eab22ab77be686
peterk <tiglek@gmail.com>
2019-07-07 06:35:54 +0900

    Updates

---
+++ precache-manifest.797a676afafdede2eb5b2f4cb8ee921d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4baf56208049e3791bd601084977224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908cfe3f9e8faa2d8553",
+    "url": "/static/css/main.1e0baa1b.chunk.css"
+  },
+  {
+    "revision": "a2ccf87e4c0b15a8ba93",
+    "url": "/static/js/2.f89e058d.chunk.js"
+  },
+  {
+    "revision": "908cfe3f9e8faa2d8553",
+    "url": "/static/js/main.d28ffc15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f89c5e0a4015ec744e18f8167d1e0e9",
+    "url": "/static/media/ai-styler-logo.5f89c5e0.svg"
+  },
+  {
+    "revision": "1bbe616a98bf7bca5494f198e438b79d",
+    "url": "/static/media/privacy.1bbe616a.md"
+  },
+  {
+    "revision": "724fea550641eef14a3129516a849492",
+    "url": "/static/media/terms.724fea55.md"
+  }
+]);
